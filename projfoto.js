70e4851c3ba24b5306d0d4f3f68dd975af261523
Deborah <deborahc.monteiro@gmail.com>
2020-08-18 19:38:38 -0300

    first commit

---
+++ projfoto.js
@@ -0,0 +1,17 @@
+<meta charset="UTF-8">
+
+<h1>Aprendendo Fotografia!</h1>
+<h2>Postagens da minha evolução na fotografia.</h2>
+Para visualizar o meu IG, clique <a href="https://www.instagram.com/debbiemonteirophoto/">aqui</a>!
+<script>
+
+	alert("Seja muito bem vindo ao meu espaço! Contribua com a sua opinião sobre composição e edição de imagem.");
+	
+<figure>
+    <img src="img/eu1.png"
+        alt="Debbie Monteiro"
+        width="200"
+        height="400">
+    <figcaption>Deborah (Debbie) Monteiro</figcaption>
+</figure>
+</script>
